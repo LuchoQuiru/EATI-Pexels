816b083b96bd44346026d3f0b837318685f7188d
Lucho <Luchoburato@gmail.com>
2023-03-15 18:17:15 -0300

    Commit final

--- app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
+++ app/src/main/java/com/eati/pexels/presentation/PhotosScreen.kt
@@ -1,24 +1,61 @@
 package com.eati.pexels.presentation
 
-import androidx.compose.material.Text
+import DetailOutlinedCard
+import SearchBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.eati.pexels.domain.Photo
+import com.eati.pexels.presentation.compents.LazyRowOfPictures
 
 
 @Composable
 fun PhotosScreen(viewModel: PhotosViewModel) {
     val result by viewModel.photosFlow.collectAsState()
-
     Photos(result, viewModel::updateResults)
 }
 
 @Composable
 fun Photos(results: List<Photo>, updateResults: (String) -> Unit) {
 
-    LaunchedEffect(Unit) {
-        updateResults("architecture")
+    var selectedPhoto by remember {
+        mutableStateOf<Photo?>(null)
     }
-
-    Text(text = results.toString())
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        InitialSearch(updateResults)
+        SearchBar(updateResults)
+        Spacer(
+            modifier = Modifier
+                .height(20.dp)
+                .background(Color.Red)
+        )
+        LazyRowOfPictures(results) { selectedPhoto = it }
+        Spacer(modifier = Modifier.height(8.dp))
+        selectedPhoto?.let {
+            DetailOutlinedCard(it)
+        }
+    }
 }
+
+@Composable
+fun InitialSearch(updateResults: (String) -> Unit) {
+    LaunchedEffect(Unit) {
+        updateResults("Messi")
+    }
+}
+
+
+
+
--- app/src/main/java/com/eati/pexels/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/eati/pexels/presentation/ui/theme/Theme.kt
@@ -1,39 +1,32 @@
 package com.eati.pexels.presentation.ui.theme
 
+import android.hardware.lights.Light
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val LightColors = lightColors(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
 )
 
 @Composable
 fun EATIPexelsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
+    /*val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
-    }
+    }*/
+    val colors = LightColors
 
     MaterialTheme(
         colors = colors,
@@ -41,4 +34,5 @@ fun EATIPexelsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         shapes = Shapes,
         content = content
     )
+
 }
